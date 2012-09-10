05c962a9a642016a162408e0f23096ccacd55ae2
Matthew Riddle <mriddle89@gmail.com>
2012-09-10 19:08:55 +1000

    More chef

--- chef/cookbooks/init/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer        "Chris Aitchison"
-maintainer_email  "chris@thinknew.com.au"
-version           "0.9"
-
-
-depends           "apt"
-supports          "ubuntu"
-
--- config/deploy/helpers.rb
+++ config/deploy/helpers.rb
@@ -4,4 +4,13 @@ Capistrano::Configuration.instance(:must_exist).load do
     from = source.next_revision(current_revision)
     capture("cd #{latest_release} && #{source.local.log(from)} #{dirs.join(' ')} | wc -l").to_i > 0
   end
+
+  def remote_file_exists?(full_path)
+    'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+  end
+
+  def remote_process_exists?(pid_file)
+    return false unless remote_file_exists? pid_file
+    capture("ps -p $(cat #{pid_file}) ; true").strip.split("\n").size == 2
+  end
 end
