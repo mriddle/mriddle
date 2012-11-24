08d62ded24487a1ad90feadfc16092116e14f28e
Matthew Riddle <mriddle89@gmail.com>
2012-11-24 11:45:45 +1100

    Added ability to display post modification details

--- blog.rb
+++ blog.rb
@@ -31,10 +31,28 @@ class Post
     year = file_hash.shift
     month = file_hash.shift
     day = file_hash.shift
+    file_path = "posts/#{filename}"
+
     post.date = Date.civil(year.to_i, month.to_i, day.to_i)
     post.permalink = "/" + [ year, month, day, URI.escape(file_hash.join('_'))].join('/')
     post.title = file_hash.join(' ').capitalize
-    post.content = File.open("posts/#{filename}").read
+    post.content = File.open(file_path).read
+
+    post = check_for_modifications(post, file_path)
+  end
+
+  private
+
+  def self.check_for_modifications(post, file_path)
+    authors = `git log "#{file_path}" | grep "Author"`.split("\n")
+    dates = `git log "#{file_path}" | grep "Date"`.split("\n")
+    post.modified = false
+    if authors.size >= 2 && dates.size >= 2
+      post.modified = true
+      post.modified_by = authors.first.gsub("Author: ", "")
+      post.modified_on = Date.parse(dates.first.gsub("Date: ", ""))
+    end
     post
   end
+
 end
